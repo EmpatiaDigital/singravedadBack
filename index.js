97f8c655889930a6c1b94da271d62112817fea7e
EmpatiaDigital <empatiadigital2025@gmail.com>
2025-08-22 07:47:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+app.use(cors());
+app.use(express.json());
+app.use('/api/test', (req, res) => {
+  res.send('Hola Backend 🚀');
+});
 require("dotenv").config();
 
 const authRoute = require("./routes/authRoute");
@@ -10,11 +15,7 @@ const musicRoute = require('./routes/musicRoute');
 const app = express();
 
 // Middlewares
-app.use(cors());
-app.use(express.json());
-app.use('/api/test', (req, res) => {
-  res.send('Hola Backend 🚀');
-});
+
 // Rutas
 app.use("/api/auth", authRoute);
 app.use("/api/upload", uploadRoute);
@@ -28,6 +29,7 @@ mongoose.connect('mongodb+srv://singravedad777:singravedad777@cluster0.ri8pn3e.m
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
 
 
 
